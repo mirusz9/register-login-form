a7f631dd45fccd333be90d9eaed4a6a17b00459d
mirusz9 <adam.sztano@gmail.com>
2020-10-04 13:21:57 +0200

    started styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./LoginPage.css"
 
 const LoginPage = () => {
 	return (
-		<div>
-			<h1>Log in</h1>
-			<div>
+		<div className="container">
+			<h1 className="title">Log in</h1>
+			<div className="loginDiv">
 				<form>
-					<label for="username">Username</label>
-					<input type="text" name="username" id="username" />
-					<label for="password">Password</label>
+					<label htmlFor="username">Username</label><br/>
+					<input type="text" name="username" id="username" /><br/>
+					<label htmlFor="password">Password</label><br/>
 					<input type="password" name="password" id="password" />
 				</form>
 			</div>
