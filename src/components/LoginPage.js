9e6bcd451980a597efd140c40d68adb55b5a3848
mirusz9 <adam.sztano@gmail.com>
2020-10-04 14:56:06 +0200

    login page css done

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./LoginPage.css"
+import './LoginPage.css';
 
 const LoginPage = () => {
 	return (
@@ -8,13 +8,19 @@ const LoginPage = () => {
 			<h1 className="title">Log in</h1>
 			<div className="loginDiv">
 				<form>
-					<label htmlFor="username">Username</label><br/>
-					<input type="text" name="username" id="username" /><br/>
-					<label htmlFor="password">Password</label><br/>
+					<label htmlFor="username">Username</label>
+					<br />
+					<input type="text" name="username" id="username" />
+					<br />
+					<label htmlFor="password">Password</label>
+					<br />
 					<input type="password" name="password" id="password" />
+					<button type="submit">Log in</button>
 				</form>
 			</div>
-			<Link to="/register">Register</Link>
+			<div className="registerDiv">
+				<Link id="register" to="/register">Create an account.</Link>
+			</div>
 		</div>
 	);
 };
